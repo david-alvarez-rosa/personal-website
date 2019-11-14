68602364111838348090e0b83c6e5b3c0b65c911
David Álvarez Rosa <david@alvarezrosa.com>
2019-11-14 19:49:44 +0100

    Add Vim-like commands for scrolling and more.

--- shortcuts.js
+++ shortcuts.js
@@ -24,7 +24,47 @@ function keyboardShortcuts(event) {
         window.scrollTo(0, 0);
         ++emacsCommandCounter;
         break;
+
+        // Vim-like commands.
+    case "j":
+        window.scrollBy(0, scrollStep);
+        ++vimCommandCounter;
+        break;
+    case "k":
+        window.scrollBy(0, -scrollStep);
+        ++vimCommandCounter;
+        break;
+    case "g":
+        window.scrollTo(0, 0);
+        ++vimCommandCounter;
+        break;
+    case "G":
+        window.scrollBy(0, document.documentElement.offsetHeight);
+        ++vimCommandCounter;
+        break;
+    case "q":
+        closeAll();
+        ++vimCommandCounter;
+        break;
+    case "d":
+        window.scrollBy(0, 3 * scrollStep);
+        ++vimCommandCounter;
+        break;
+    case "u":
+        window.scrollBy(0, -3 * scrollStep);
+        ++vimCommandCounter;
+        break;
+
+        // Other commands.
+    case "m":
+        toggleNavBar();
+        break;
+    case "Escape":
+        closeAll();
+        break;
     }
+
+    showInformation();
 }
 
 window.addEventListener("keydown", keyboardShortcuts);
