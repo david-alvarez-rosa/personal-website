2562e4a9a9fa18da3e7a48e345eb4ebc0705844f
David Álvarez Rosa <david@alvarezrosa.com>
2020-05-02 16:23:45 +0200

    Use minified version of chart.js.

--- index.php
+++ index.php
@@ -1393,7 +1393,7 @@
 
 
     <!-- Javascript files. -->
-    <script src="js/chart.js"></script>
+    <script src="js/chart.min.js"></script>
     <script src="js/main.js"></script>
     <script src="js/home.js"></script>
     <script src="js/shortcuts.js"></script>
