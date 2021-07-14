3d32ca38324f22378bf21654ec90758cbb0318b0
David Álvarez Rosa <david@alvarezrosa.com>
2021-07-14 18:06:23 +0200

    Add preferred gener pronouns and (beautiful) pride flag

--- resources/header.php
+++ resources/header.php
@@ -22,61 +22,73 @@
 
 
 <header>
-   <?php include "backgroundImage.php" ?>
-   <a href="https://david.alvarezrosa.com"
-      title="David Álvarez Rosa's personal website.">
-      <h1>David Álvarez Rosa</h1>
-   </a>
-   <h2>
-      <strong>Mathematics</strong> and <br />
-      <strong>Industrial Engineering</strong> graduate.
-   </h2>
-   <div id="sites">
-      <a href="https://david.alvarezrosa.com/about"
-         <?php if ($currentSite === 'about') echo 'class="currentSite"'; ?>
-         title="About page of David Álvarez Rosa's personal website.">
-         About <i class="shine shineHeader1"></i>
-      </a>
-      <a href="https://blog.alvarezrosa.com"
-         <?php if ($currentSite === 'blog') echo 'class="currentSite"'; ?>
-         title="David Álvarez Rosa's personal blog.">
-         Blog <i class="shine shineHeader2"></i>
-      </a>
-      <a href="/pdf/cv-david-alvarez-rosa.pdf#view=Fit"
-         title="My personal Curriculum Vitae.">
-         CV <i class="shine shineHeader3"></i>
-      </a>
-   </div>
-   <div id="mediaControl" class="controllers">
-      <button id="backward"
-              title="Show previous image."
-              onclick="backwardAnimation();">
-         <i class="fas fa-step-backward"></i>
-      </button>
-      <button id="pause"
-              title="Toggle background animation state."
-              onclick="toggleAnimation();">
-         <i id="toggleIcon" class="fas fa-pause"></i>
-      </button>
-      <button id="forward"
-              title="Show next image."
-              onclick="forwardAnimation();">
-        <i class="fas fa-step-forward"></i>
-      </button>
-   </div>
-   <div id="downloadContainer" class="controllers">
-     <button id="download"
-             title="Download full size background image."
-             onclick="downloadBackground();">
-       <i class="fas fa-download"></i>
-     </button>
-   </div>
-   <div id="themeContainer" class="controllers">
-     <button id="theme"
-             title="Do you think this website is too complex and you prefer
-                 minimalism? Change to a theme that suck less."
-             onclick="toggleTheme()";>
-       <i class="fas fa-brush"></i>
-     </button>
-   </div>
+  <?php include "backgroundImage.php" ?>
+  <div id="pronounsContainer">
+    <div id="pronounsContainerAux">
+      <div id="prideFlag"></div>
+      <div id="pronouns">
+        <a href="https://en.wikipedia.org/wiki/Preferred_gender_pronoun"
+           title="Preferred gender pronoun - Wikipedia.">
+          (He/Him)
+        </a>
+      </div>
+    </div>
+  </div>
+  <a id="name"
+     href="https://david.alvarezrosa.com"
+     title="David Álvarez Rosa's personal website.">
+    <h1>David Álvarez Rosa</h1>
+  </a>
+  <h2>
+    <strong>Mathematics</strong> and <br />
+    <strong>Industrial Engineering</strong> graduate.
+  </h2>
+  <div id="sites">
+    <a href="https://david.alvarezrosa.com/about"
+       <?php if ($currentSite === 'about') echo 'class="currentSite"'; ?>
+       title="About page of David Álvarez Rosa's personal website.">
+      About <i class="shine shineHeader1"></i>
+    </a>
+    <a href="https://blog.alvarezrosa.com"
+       <?php if ($currentSite === 'blog') echo 'class="currentSite"'; ?>
+       title="David Álvarez Rosa's personal blog.">
+      Blog <i class="shine shineHeader2"></i>
+    </a>
+    <a href="/pdf/cv-david-alvarez-rosa.pdf#view=Fit"
+       title="My personal Curriculum Vitae.">
+      CV <i class="shine shineHeader3"></i>
+    </a>
+  </div>
+  <div id="mediaControl" class="controllers">
+    <button id="backward"
+            title="Show previous image."
+            onclick="backwardAnimation();">
+      <i class="fas fa-step-backward"></i>
+    </button>
+    <button id="pause"
+            title="Toggle background animation state."
+            onclick="toggleAnimation();">
+      <i id="toggleIcon" class="fas fa-pause"></i>
+    </button>
+    <button id="forward"
+            title="Show next image."
+            onclick="forwardAnimation();">
+      <i class="fas fa-step-forward"></i>
+    </button>
+  </div>
+  <div id="downloadContainer" class="controllers">
+    <button id="download"
+            title="Download full size background image."
+            onclick="downloadBackground();">
+      <i class="fas fa-download"></i>
+    </button>
+  </div>
+  <div id="themeContainer" class="controllers">
+    <button id="theme"
+            title="Do you think this website is too complex and you prefer
+                minimalism? Change to a theme that suck less."
+            onclick="toggleTheme()";>
+      <i class="fas fa-brush"></i>
+    </button>
+  </div>
 </header>
