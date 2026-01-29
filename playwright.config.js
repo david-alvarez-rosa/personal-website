6d119f583978dcb1ad2ad50af6b38b46678fa074
David Álvarez Rosa <david@alvarezrosa.com>
2026-01-29 21:56:00 +0000

    Simplify playwright pipeline workflow

--- playwright.config.js
+++ playwright.config.js
@@ -1,33 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const isCI = !!process.env.CI;
-
-const baseProjects = [
-  {
-    name: 'Desktop Chrome',
-    use: { ...devices['Desktop Chrome'] },
-  },
-  {
-    name: 'Desktop Firefox',
-    use: { ...devices['Desktop Firefox'] },
-  },
-  {
-    name: 'Mobile Chrome',
-    use: { ...devices['Pixel 5'] },
-  },
-];
-
-const safariProjects = [
-  {
-    name: 'Desktop Safari',
-    use: { ...devices['Desktop Safari'] },
-  },
-  {
-    name: 'Mobile Safari',
-    use: { ...devices['iPhone 12'] },
-  },
-];
-
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
@@ -36,7 +8,20 @@ export default defineConfig({
     baseURL: 'http://localhost:1313',
     screenshot: 'only-on-failure',
   },
-  projects: isCI ? [...baseProjects, ...safariProjects] : baseProjects,
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+  ],
   webServer: {
     command: 'hugo server',
   },
