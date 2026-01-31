a51fca5b65dbb2315e33f91af00518074b13e55d
David Álvarez Rosa <david@alvarezrosa.com>
2026-01-31 14:43:29 +0000

    Add /404 and /subscription pages to testing suite

--- tests/config.js
+++ tests/config.js
@@ -2,6 +2,8 @@ export const PAGES = [
   { url: '/', name: 'homepage' },
   { url: '/about/', name: 'about' },
   { url: '/posts/', name: 'posts' },
+  { url: '/404/', name: '404' },
+  { url: '/subscription/', name: 'subscription' },
 ];
 
 export const VIEWPORTS = {
