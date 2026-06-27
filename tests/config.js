b77c2b8d9752353512beb6c1a8bd94205fc3f618
David Álvarez Rosa <david@alvarezrosa.com>
2026-06-27 14:34:06 +0100

    Simplification of layouts and consistent aesthetics

--- tests/config.js
+++ tests/config.js
@@ -2,6 +2,7 @@ export const PAGES = [
   { url: '/', name: 'homepage' },
   { url: '/about/', name: 'about' },
   { url: '/posts/', name: 'posts' },
+  { url: '/tags/performance/', name: 'tag-performance' },
   { url: '/404/', name: '404' },
   { url: '/subscription/', name: 'subscription' },
 ];
